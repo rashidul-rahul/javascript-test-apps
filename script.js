018433ec7ea5e4bd1f1dfc53953b4be6bac09498
rashidul rahul <rashidul.rahul@goama.com>
2022-02-16 21:31:12 +0600

    Complete the guess game

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+const randomNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20
 
-console.log(document.querySelector(".guess").value)
+function checkNumber(num) {
+	if (!num) {
+		return "No a Number!"
+	}else if (num > randomNumber) {
+		score --;
+		return "High guess";
+	} else if (num < randomNumber) {
+		score--;
+		return "Low guess";
+	}
+	document.querySelector(".number").textContent = num;	
+	return "Perfect";
+}
+
+document.querySelector(".check").addEventListener('click', function(){
+	const message = checkNumber(document.querySelector(".guess").value)
+	// console.log(document.querySelector(".score").textContent)
+	document.querySelector(".score").textContent = score;
+	document.querySelector(".message").textContent = message;
+})
